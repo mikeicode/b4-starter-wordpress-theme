ecca461bc2f104c6eb1780e9b267becde88c6107
mikeicode <adam19865944rf@gmail.com>
2015-01-29 17:47:27 -0500

    remove sidebar

--- 404.php
+++ 404.php
@@ -8,7 +8,6 @@ get_header();
 get_template_part( 'menu', 'index' ); ?>
 
 	<div class="container">
-
       	<div class="row">
         	<article id="post-<?php the_ID(); ?>" class="col-xs-12 mt50 mb50 align-center">
 				<p>Sorry, you are looking for something that isn't here. Please use the navigation above.</p>
@@ -16,6 +15,4 @@ get_template_part( 'menu', 'index' ); ?>
 		</div>
 	</div>
         
-
-<?php get_template_part( 'sidebar', 'index' ); ?>
 <?php get_footer(); ?>
