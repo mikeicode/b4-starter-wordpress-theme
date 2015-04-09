67af835fcc4d82dfc7bdc046acbb52f196369e1c
mikeicode <adam19865944rf@gmail.com>
2015-04-09 09:48:20 -0400

    Cleanup search page

--- search.php
+++ search.php
@@ -7,23 +7,38 @@
 	get_template_part( 'menu', 'index' );  
 ?>  
 
-       <div class="two-thirds column alpha">
-              <?php if ( have_posts() ) : ?>
-                   <h2 class="page-title"><?php printf( __( 'Search Results for: %s', 'mb' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
-                   
-                      <?php get_template_part( 'loop', 'search' ); ?>
-                   
-						<?php else : ?>
-                                <div id="post-0" class="post no-results not-found">
-                                    <h2 class="entry-title"><?php _e( 'Nothing Found', 'mb' ); ?></h2>
-                                    <div class="entry-content">
-                                        <p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyten' ); ?></p>
-                                        <?php get_search_form(); ?>
-                                    </div><!-- .entry-content -->
-                                </div><!-- #post-0 -->
-                        <?php endif; ?>
-                   
-</div>
+	<div class="container">
 
-<?php get_template_part( 'sidebar', 'index' );  ?>
+        <!-- left and right columns -->
+      	<div class="row">
+    	
+        	<!-- main article - left column -->
+        	<article class="col-xs-12 col-sm-9">
+        		
+				<?php if ( have_posts() ) : ?>
+              
+                   <h1 class="mb25"><?php printf( __( 'Search Results for: %s', 'mb' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+                   
+                   	<ul class="search-list">
+                    	<?php while ( have_posts() ) : the_post(); ?>
+                    	<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                    	<?php endwhile; ?>
+                    </ul>
+                    
+					<?php else : ?>
+                               <h1 class="title mb25"><?php printf( __( 'Search Results for: %s', 'mb' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+                               <p style="font-size:15px">Sorry, but nothing matched your search criteria.<br>Please try again or use the navigation above.</p>
+                    <?php endif; ?>
+                        
+        	</article><!-- /main article - left column --> 
+        
+        	<!-- right column sidebar -->
+        	<div class="col-xs-12 col-sm-3">
+          		<?php get_template_part( 'sidebar' ); ?>
+        	</div><!-- /right column sidebar -->
+        
+      	</div><!-- /left and right columns -->
+
+	</div> <!-- /container -->
+    
 <?php get_footer(); ?>
