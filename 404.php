1901db4153e22eb1a1e6befb7f6f0c07b3a2bf25
Mike Ilsley <mikeilsley@gmail.com>
2018-08-15 12:39:31 -0400

    Add 404 to advanced custom fields in theme

--- 404.php
+++ 404.php
@@ -5,9 +5,8 @@ get_template_part( 'menu', 'index' ); ?>
 	<section class="main-404">
         <div class="container">
             <div class="row">
-                <article id="post-<?php the_ID(); ?>" class="col-xs-12 mt50 mb50 align-center">
-                    <p>Sorry, you are looking for something that isn't here.</p>
-                    <p><a href="/" class="button">Visit Homepage</a></p>
+                <article id="post-<?php the_ID(); ?>" class="col mt50 mb50 align-center">
+                    <?php the_field( '404_page_content', 'option' ); ?>
                 </article>
             </div>
         </div>
--- inc/custom-fields.php
+++ inc/custom-fields.php
@@ -46,4 +46,52 @@ acf_add_local_field_group(array(
 
 endif;
 
+/* 404 page */
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_5930aec544963',
+	'title' => '404 Page',
+	'fields' => array(
+		array(
+			'key' => 'field_5930aecab95d3',
+			'label' => '404 Page Content',
+			'name' => '404_page_content',
+			'type' => 'wysiwyg',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+			'delay' => 0,
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'options_page',
+				'operator' => '==',
+				'value' => 'acf-options-404-page',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+endif;
+
 ?>
