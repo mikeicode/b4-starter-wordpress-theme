ee41b5fe1447799ae7d01a99c09f0f021a997bcd
Mike Ilsley <mikeilsley@gmail.com>
2018-08-16 10:32:33 -0400

    Add CFS favicon

--- header.php
+++ header.php
@@ -15,11 +15,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title><?php wp_title(); ?></title>
 
-	<!-- Favicons
+	<!-- Favicon
 	==================================================  -->
-    
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png">
-	
+   	<?php $site_favicon = get_field( 'site_favicon', 'option' ); ?>
+	<?php if ( $site_favicon ) { ?>
+		<link rel="shortcut icon" href="<?php echo $site_favicon['url']; ?>">
+	<?php } ?>
   
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -28,8 +29,6 @@
     <![endif]-->
 
 <?php wp_head(); ?>  
-
- 
 </head>
 
 <?php the_field( 'header_code', 'option' ); ?>   
@@ -37,5 +36,4 @@
 
 <body <?php body_class(); ?>>
 <?php the_field( 'body_code', 'option' ); ?> 
-<div id="page-wrap">
-
+<div id="page-wrap"><!-- this ID is for mmenu mobile nav -->
