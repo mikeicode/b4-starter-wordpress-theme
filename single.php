0e65bea162c060bb03b3384688768fd40d69ec62
Mike Ilsley <mikeilsley@gmail.com>
2018-08-16 08:40:29 -0400

    Style meta, make one column page

--- single.php
+++ single.php
@@ -4,45 +4,27 @@
 ?>      
     
     <div class="container">
-
-        <!-- left and right columns -->
       	<div class="row"> 
-        
-        <!-- left column -->   	
-        <div class="col-xs-12 col-sm-9">
-        
-			<?php while ( have_posts() ) : the_post(); ?> <!--  the loop -->
-                        
-        	<article id="post-<?php the_ID(); ?>">
-          		<h1 class="post-title"><?php the_title(); ?></h1>
-             
-            	<?php the_content(); ?>
-                 <?php edit_post_link(); ?> 
-	 
-             	<!-- the meta-->   
-              	<div class="meta"> 
-                    	Date posted: <?php echo get_the_date(); ?>
-                  	| Author: <?php the_author_posts_link(); ?>
-                  	| <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-                  <p>Categories: <?php the_category(' '); ?></p>
-              	</div><!-- /the meta--> 
-                
-        		<?php if(is_page() || is_single()) : comments_template( '', true ); endif; ?>
-        
-        	</article>
+			<div class="col">
+				<?php while ( have_posts() ) : the_post(); ?> <!--  the loop -->
 
-			<?php endwhile; ?><!-- /the loop -->
+					<article id="post-<?php the_ID(); ?>">
+						<h1 class="post-title"><?php the_title(); ?></h1>
 
-          
-      	</div><!-- /left column -->
-        
-        	<!-- right column sidebar -->
-        	<div class="col-xs-12 col-sm-3" id="sidebar">
-          		<?php get_template_part( 'sidebar' ); ?>
-        	</div><!-- /right column sidebar -->
-        
-      	</div><!-- /left and right columns -->
+						<?php the_content(); ?>
 
-    </div> <!-- /container -->
+						<!-- meta-->   
+						<div class="meta"> 
+							<p>Posted: <?php echo get_the_date(); ?> in: <?php the_category(', '); ?></p>
+						</div><!-- /meta--> 
+
+						<?php if(is_page() || is_single()) : comments_template( '', true ); endif; ?>
+
+					</article>
+
+				<?php endwhile; ?><!-- /the loop -->
+			</div>
+      	</div><!-- /row -->
+    </div><!-- /container -->
            
 <?php get_footer(); ?>  
