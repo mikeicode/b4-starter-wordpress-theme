8e397e02e2a0bcb867df0d1a676c45a6c8dad0fa
Mike Ilsley <mikeilsley@gmail.com>
2018-08-17 10:24:36 -0400

    Clean up background properties and container padding, margin inline CSS

--- inc/modules/copy-module.php
+++ inc/modules/copy-module.php
@@ -1,45 +1,37 @@
 
-
-<section class="copy-section <?php the_sub_field( 'custom_css_classes' ); ?>" style="
-
-<?php if ( have_rows( 'copy_module_padding' ) ) : ?>
-	<?php while ( have_rows( 'copy_module_padding' ) ) : the_row(); ?>
-        padding-top:<?php the_sub_field( 'padding_top' ); ?>px;
-        padding-bottom:<?php the_sub_field( 'padding_bottom' ); ?>px;
-    <?php endwhile; ?>
-<?php endif; ?>
-
-<?php if ( have_rows( 'copy_module_margin' ) ) : ?>
-	<?php while ( have_rows( 'copy_module_margin' ) ) : the_row(); ?>
-    	margin-top:<?php the_sub_field( 'margin_top' ); ?>px;
-        margin-top:<?php the_sub_field( 'margin_bottom' ); ?>px;
-    <?php endwhile; ?>
-<?php endif; ?>
-
-<?php if ( get_sub_field( 'copy_module_background_color' ) )  {
-		echo 'background-color:';
-		the_sub_field( 'copy_module_background_color' );
-		echo ';';
-	}
-?>
-
-<?php if ( get_sub_field( 'copy_module_background_image' ) )  {
-	echo 'background-image:url(';
-	the_sub_field( 'copy_module_background_image' );
-	echo ');';
-	}
-?>
-
-<?php if ( have_rows( 'copy_module_background_properties' ) ) : ?>
-	<?php while ( have_rows( 'copy_module_background_properties' ) ) : the_row(); ?>
-        background-position:<?php the_sub_field( 'background_image_position' ); ?>;
-        background-size:<?php the_sub_field( 'background_image_size' ); ?>;
-        background-repeat:<?php the_sub_field( 'background_image_repeat' ); ?>;
-    <?php endwhile; ?>
-<?php endif; ?>
-">
-
 <?php 
+	$custom_css_classes = get_sub_field( 'custom_css_classes');
+				  
+	if ( have_rows( 'copy_module_padding' ) ) :
+		 while ( have_rows( 'copy_module_padding' ) ) : the_row(); 
+			 $padding_top =  get_sub_field( 'padding_top' ); 
+			 $padding_bottom =  get_sub_field( 'padding_bottom' ); 
+		 endwhile; 
+	endif; 
+
+	if ( have_rows( 'copy_module_margin' ) ) :
+		 while ( have_rows( 'copy_module_margin' ) ) : the_row(); 
+			 $margin_top =  get_sub_field( 'margin_top' ); 
+			 $margin_bottom =  get_sub_field( 'margin_bottom' ); 
+		 endwhile; 
+	endif;
+
+	if ( get_sub_field( 'copy_module_background_color' ) )  {
+		$copy_module_background_color = get_sub_field( 'copy_module_background_color' );
+	}
+
+	if ( get_sub_field( 'copy_module_background_image' ) )  {
+		$copy_module_background_image = get_sub_field( 'copy_module_background_image' );
+	}
+
+	if ( have_rows( 'copy_module_background_properties' ) ) : 
+		while ( have_rows( 'copy_module_background_properties' ) ) : the_row();
+			$background_image_position = get_sub_field( 'background_image_position' ); 
+			$background_image_size = get_sub_field( 'background_image_size' ); 
+			$background_image_repeat = get_sub_field( 'background_image_repeat' ); 
+		endwhile; 
+	 endif; 
+
 	if ( have_rows( 'layout' ) ) :
 		while ( have_rows( 'layout' ) ) : the_row(); 
 			$containerClasses =  get_sub_field( 'container_width' ); 
@@ -47,22 +39,50 @@
 		endwhile; 
 	endif;
 ?>
-	
+
+<section class="copy-section <?php if (!empty($custom_css_classes)) {echo $custom_css_classes;}?>" style="
+
+<?php
+	if (!empty($padding_top)) {
+		echo 'padding-top:'. $padding_top .'px;';
+	} 
+	if (!empty($padding_bottom)) {
+		echo 'padding-bottom:'. $padding_bottom .'px;';
+	}
+	if (!empty($margin_top)) {
+		echo 'margin-top:'. $margin_top .'px;';
+	}
+	if (!empty($margin_bottom)) {
+		echo 'margin-bottom:'. $margin_bottom .'px;';
+	}
+	if (!empty($copy_module_background_color)) {
+		echo 'background-color:'. $copy_module_background_color .';';
+	}
+	if (!empty($copy_module_background_image)) {
+		echo 'background-image:url('. $copy_module_background_image .');';
+	}
+	if (!empty($background_image_position)) {
+		echo 'background-position:'. $background_image_position .';';
+	}
+	if (!empty($background_image_size)) {
+		echo 'background-size:'. $background_image_size .';';
+	}
+	if (!empty($background_image_repeat)) {
+		echo 'background-repeat:'. $background_image_repeat .';';
+	}
+?>																						 
+">
 
 	<div class="<?php echo $containerClasses ?>">
 		<div class="row <?php echo $alignmentClasses ?>">
 			<?php if ( have_rows( 'copy_columns' ) ) : ?>
 				<?php while ( have_rows( 'copy_columns' ) ) : the_row(); ?>
 					<div class="<?php the_sub_field( 'column_width' ); ?>">
-						
 						<?php the_sub_field( 'column_content' ); ?>
 					</div>
 				<?php endwhile; ?>
-			<?php else : ?>
-				<?php // no rows found ?>
 			<?php endif; ?>
-		</div>
-	</div>
+		</div><!-- /.row -->
+	</div><!-- /.container -->
 	
  </section>
-
