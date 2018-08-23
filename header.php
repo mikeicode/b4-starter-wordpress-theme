a7e8db00043e4fced5b493880d171ec5a09b3b3b
Mike Ilsley <mikeilsley@gmail.com>
2018-08-23 13:18:13 -0400

    Updated default meta title tag to format  Page Name | Site Name

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 	<!-- Mobile Specific Metas
   ================================================== -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title><?php wp_title(); ?></title>
+    <title><?php wp_title( '|', true, right ); bloginfo( 'name' ); ?></title>
 
 	<!-- Favicon
 	==================================================  -->
